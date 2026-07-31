f6439b7d7279803df2bc3b9c3e2bcb9d77fc2b7b
BoneWalk3r <thecrimsonknight1230@proton.me>
2026-07-30 20:18:35 -0400

    Forcing structuredExtraConfig options.

--- flake.nix
+++ flake.nix
@@ -229,19 +229,19 @@
             pkgs.linuxPackagesFor (
               pkgs.linuxPackages_latest.kernel.override {
                 structuredExtraConfig = with pkgs.lib.kernel; {
-                  MT76_CORE = no;
-                  MT76_CONNAC_LIB = no;
-                  MT792x_LIB = no;
+                  MT76_CORE = lib.mkForce no;
+                  MT76_CONNAC_LIB = lib.mkForce no;
+                  MT792x_LIB = lib.mkForce no;
 
-                  MT7921_COMMON = no;
-                  MT7921E = no;
+                  MT7921_COMMON = lib.mkForce no;
+                  MT7921E = lib.mkForce no;
 
-                  MT7925_COMMON = no;
-                  MT7925E = no;
+                  MT7925_COMMON = lib.mkForce no;
+                  MT7925E = lib.mkForce no;
 
-                  BT_HCIBTUSB_MTK = no;
-                  BT_HCIBTUSB = no;
-                  BT_MTK = no;
+                  BT_HCIBTUSB_MTK = lib.mkForce no;
+                  BT_HCIBTUSB = lib.mkForce no;
+                  BT_MTK = lib.mkForce no;
                 };
               }
             );
