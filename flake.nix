b1c06986a200950be7f7d310f11cef8ca852e6ad
BoneWalk3r <thecrimsonknight1230@proton.me>
2026-08-27 23:27:15 -0400

    Updated wifiPatches.

--- flake.nix
+++ flake.nix
@@ -57,6 +57,7 @@
             "mt7927-wifi-10-disable-aspm-and-runtime-pm-for-mt7927.patch"
             "mt7927-wifi-11-replace-ismt7925-with-isconnac3.patch"
             "mt7927-wifi-12-use-link-specific-removal-for-non-mld-st.patch"
+            "mt7927-wifi-13-tolerate-inactive-bss-deactivation.patch"
             "mt7927-wifi-23-keep-tx-ba-state-in-the-primary-wcid.patch"
             # add others as discovered
           ];
