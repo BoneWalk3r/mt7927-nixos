f36b63d3404d04bb8bae15f5315989311dbec0d6
BoneWalk3r <thecrimsonknight1230@proton.me>
2026-08-27 23:34:53 -0400

    Updated wifiPatches.

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@
             "mt7927-wifi-24-pass-wcid-explicitly-to-mt7925mcustaba.patch"
             "mt7927-wifi-25-program-ba-state-on-active-links.patch"
             "mt7927-wifi-26-use-a-helper-to-cache-txpowercur.patch"
+            "mt7927-wifi-27-factor-out-rate-power-limit-calculation.patch"
             # add others as discovered
           ];
 
