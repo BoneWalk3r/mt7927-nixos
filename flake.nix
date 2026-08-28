ac78f2403b97bd2b5726fc85475428cfc57c2efd
BoneWalk3r <thecrimsonknight1230@proton.me>
2026-08-27 23:23:11 -0400

    Updated wifiPatches.

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
             "mt7927-wifi-02-fix-stale-pointer-comparisons-in-changev.patch"
             "mt7927-wifi-05-populate-eht-320mhz-mcs-map-in-starec.patch"
             "mt7927-wifi-06-advertise-eht-320mhz-capabilities-for-6g.patch"
+            "mt7927-wifi-08-add-mt7927-firmware-paths.patch"
             "mt7927-wifi-23-keep-tx-ba-state-in-the-primary-wcid.patch"
             # add others as discovered
           ];
